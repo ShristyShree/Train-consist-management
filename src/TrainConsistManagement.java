5c6c04464e6eb71ec047829d7697e34384600bc7
Hy-Shravan <shravan.per26@gmail.com>
2026-04-07 14:20:43 +0530

    UC19: Binary Search for Bogie ID (Optimized Searching)

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -1,25 +1,43 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class TrainConsistManagement{
+public class TrainConsistManagement {
 
-    public static boolean linearSearch(String[] bogieIds, String key) {
-        for (int i = 0; i < bogieIds.length; i++) {
-            if (bogieIds[i].equals(key)) {
+    public static boolean binarySearch(String[] bogieIds, String key) {
+        if (bogieIds.length == 0) {
+            return false;
+        }
+
+        Arrays.sort(bogieIds);
+
+        int low = 0;
+        int high = bogieIds.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int result = key.compareTo(bogieIds[mid]);
+
+            if (result == 0) {
                 return true;
+            } else if (result < 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
+
         return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] bogieIds = {"BG101","BG205","BG309","BG412","BG550"};
+        String[] bogieIds = {"BG309","BG101","BG550","BG205","BG412"};
 
         System.out.println("Enter bogie ID to search:");
         String key = sc.nextLine();
 
-        boolean found = linearSearch(bogieIds, key);
+        boolean found = binarySearch(bogieIds, key);
 
         if (found) {
             System.out.println("Bogie ID found");
